eb7eb34a10ef14f0831ceba2a2befc8887944ca5
Sean benhur <43300345+seanbenhur@users.noreply.github.com>
2020-06-27 20:24:44 +0530

    Update demo_client.py

--- facerec/demo_client.py
+++ facerec/demo_client.py
@@ -4,8 +4,8 @@ import json
 def test_face_match():
     url = 'http://127.0.0.1:5001/face_match'
     # open file in binary mode
-    files = {'file1': open('sample_images/peter.jpg', 'rb'),
-             'file2': open('sample_images/peter2.jpg', 'rb')}     
+    files = {'file1': open('sample_images/stark.jpg', 'rb'),
+             'file2': open('sample_images/stark2.jpg', 'rb')}     
     resp = requests.post(url, files=files)
     print( 'face_match response:\n', json.dumps(resp.json()) )
     
@@ -13,8 +13,7 @@ def test_face_rec():
     url = 'http://127.0.0.1:5001/face_rec'
     # open file in binary mode
     files = {'file': open('sample_images/peter2.jpg', 'rb')} 
-    params = {'facial_features': 'true', 'face_locations':'true'}
-    resp = requests.post(url, files = files, params = params)
+    resp = requests.post(url, files = files)
     print( 'face_rec response:\n', json.dumps(resp.json()) )
     
 def main():
