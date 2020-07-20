f8b5988425185ed3c85872b98b622932895f932b
Sean benhur <43300345+seanbenhur@users.noreply.github.com>
2020-07-20 11:13:46 +0530

    Commented face match

--- facerec/demo_client.py
+++ facerec/demo_client.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-def test_face_match():
+"""def test_face_match():
     url = 'http://127.0.0.1:5001/face_match'
     # open file in binary mode
     files = {'file1': open('sample_images/stark.jpg', 'rb'),
              'file2': open('sample_images/justin.jpg', 'rb')}     
     resp = requests.post(url, files=files)
-    print( 'face_match response:\n', json.dumps(resp.json()) )
+    print( 'face_match response:\n', json.dumps(resp.json()) )"""
     
 def test_face_rec():
     url = 'http://127.0.0.1:5001/face_rec'
